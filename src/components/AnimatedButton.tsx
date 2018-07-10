3119f0da5269dca1c0e5da9f354abb2bbd4edb51
Andrew Hickey <andrew.hickey@alfasystems.com>
2018-07-10 18:38:47 +0100

    Animates border path on hover

--- src/components/AnimatedButton.tsx
+++ src/components/AnimatedButton.tsx
@@ -1,50 +1,91 @@
 import * as React from 'react'
-import { Component } from 'react'
+import { PureComponent } from 'react'
+import { keyframes } from 'styled-components'
+import { Motion, spring } from 'react-motion'
 
-class AnimatedButton extends Component {
+const drawPath = keyframes`
+  to {
+    stroke-dashoffset: 1000;
+  }
+`
+
+class AnimatedButton extends PureComponent {
   textRef = React.createRef<SVGTextElement>()
   borderRef = React.createRef<SVGPathElement>()
 
-  state = { width: 10, height: 10 }
+  state = { width: 10, height: 10, borderLength: 0, hasFocus: false }
 
   componentDidMount() {
-    this.resizeToText()
+    this._resizeToText()
   }
 
   componentDidUpdate() {
-    this.updatePathLength()
+    this._updatePathLength()
   }
 
-  updatePathLength = () => {
+  _updatePathLength = () => {
     const border = this.borderRef.current
-
     const borderLength = border.getTotalLength()
     this.setState({ borderLength })
   }
 
-  resizeToText = () => {
+  _resizeToText = () => {
     const text = this.textRef.current
     const { width, height } = text.getBBox()
     this.setState({ width, height })
   }
 
+  _setFocus = () => {
+    this.setState({ hasFocus: true })
+  }
+
+  _setBlur = () => {
+    this.setState({ hasFocus: false })
+  }
+
   render() {
-    const { width, height } = this.state
+    const { width, height, borderLength, hasFocus } = this.state
+    const targetOffset = hasFocus ? 0 : borderLength
 
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
-        <path
-          d={`M0 0 H${width} V${height} H0 L0 0`}
-          style={{ stroke: 'black', fill: 'none', strokeDasharray: '10px' }}
-        />
-        <text
-          dy="50%"
-          dominantBaseline="central"
-          style={{ stroke: 'black', fill: 'none', strokeDasharray: '1px' }}
-          ref={this.textRef}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        onMouseEnter={this._setFocus}
+        onMouseLeave={this._setBlur}
+      >
+        <Motion
+          defaultStyle={{ dashOffset: 0 }}
+          style={{ dashOffset: spring(targetOffset) }}
         >
-          TEXT!
-        </text>
+          {({ dashOffset }) => (
+            <>
+              <path
+                ref={this.borderRef}
+                d={`M0 0 H${width} V${height} H0 L0 0`}
+                style={{
+                  stroke: 'black',
+                  fill: 'none',
+                  strokeWidth: '4px',
+                  strokeDashoffset: dashOffset,
+                  strokeDasharray: borderLength,
+                }}
+              />
+              <text
+                ref={this.textRef}
+                dy="50%"
+                dominantBaseline="central"
+                style={{
+                  stroke: 'black',
+                  fontSize: '30px',
+                }}
+              >
+                TEXT!
+              </text>
+            </>
+          )}
+        </Motion>
       </svg>
     )
   }
