ca664b04addb8f1ced6cdb1c1d665300e2db161b
Andrew Hickey <andrewhickey@live.co.uk>
2019-03-26 16:09:40 +0000

    Changes font to Inconsolata

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,7 +1,14 @@
 import * as Typography from 'typography'
 import sutroTheme from 'typography-theme-sutro'
 
+sutroTheme.googleFonts = [
+  { name: 'Inconsolata', styles: ['300', '300i', '700', '700i'] },
+]
+sutroTheme.headerFontFamily = ['Inconsolata', 'sans-serif']
+sutroTheme.bodyFontFamily = ['Inconsolata', 'sans-serif']
+
 const typography = new Typography(sutroTheme)
+
 const { rhythm, scale } = typography
 
 export { rhythm, scale }
