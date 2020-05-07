a08a0c2b3a2e60a83aab0c6e722570e7a9d84104
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-07 07:16:00 +0100

    Flips using react-flip-toolkit

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,44 +1,62 @@
 import * as React from 'react'
 import { useCallback, useMemo, useState } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
-import { animated, interpolate, useSprings } from 'react-spring'
 import { ResumeSchema } from '../../types/ResumeSchema'
 import { color1, color2, color3 } from '../../utils/colors'
 import SkillIcon from '../SkillIcon'
+import classNames from 'classnames'
+import { Flipper, Flipped } from 'react-flip-toolkit'
 
 const ITEM_WIDTH = 40
 const ITEM_HEIGHT = 60
 const BAR_MAX_WIDTH = 200
 
-type SkillItemProps = {
-  isExpanded: boolean
-  canHover: boolean
-  index: number
-  item: any
-  style?: any
-  barStyle?: any
-  textStyle?: any
-  onClick?: (index: number) => void
-}
-function SkillItem({
-  isExpanded,
-  canHover,
-  item,
-  index,
-  style,
-  barStyle,
-  textStyle,
-  onClick,
-}: SkillItemProps) {
-  const transforms = useSprings(
-    item.keywords.length,
-    item.keywords.map((keyword: string, index: number) => ({
-      transform: `translate3d(0, ${(isExpanded ? 1 : 0) *
-        (index + 1) *
-        ITEM_HEIGHT}px, 0)`,
-    }))
-  )
+const createIconFlipId = (index: number) => `icon-${index}`
+const createSubIconFlipId = (keyword: string) => `subIcon-${keyword}`
+const createTextFlipId = (index: number) => `text-${index}`
+const createSubTextFlipId = (keyword: string) => `subText-${keyword}`
 
+const shouldFlip = (index: number) => (prev: number, current: number) =>
+  index === current
+
+type SubItemProps = {
+  index: number
+  keyword: string
+}
+function SubItem({ index, keyword }: SubItemProps) {
+  return (
+    <li className="flex flex-col items-end">
+      <Flipped flipId={createSubIconFlipId(keyword)} stagger="item">
+        <div
+          className="rounded-full p-2"
+          css={{
+            backgroundColor: 'white',
+            boxShadow: `0px 0px 5px 0px ${color1}`,
+          }}
+        >
+          <SkillIcon
+            skill={keyword}
+            css={{
+              stroke: color1,
+              fill: color1,
+            }}
+          />
+        </div>
+      </Flipped>
+      <Flipped flipId={createSubTextFlipId(keyword)} stagger="text">
+        <div className="text-xs whitespace-no-wrap text-right">{keyword}</div>
+      </Flipped>
+    </li>
+  )
+}
+
+type VerticalItemProps = {
+  index: number
+  onClick: (index: number) => void
+  item: any
+  isOpen: boolean
+}
+function VerticalItem({ index, onClick, item, isOpen }: VerticalItemProps) {
   const handleClick = useCallback(
     e => {
       if (onClick) {
@@ -46,113 +64,80 @@ function SkillItem({
         onClick(index)
       }
     },
-    [onClick, index]
+    [index, onClick]
   )
 
   return (
-    <animated.div
-      className={`relative flex flex-col items-end ${
-        canHover ? 'hoverable cursor-pointer' : ''
-      } ${isExpanded ? 'expanded' : ''}`}
-      css={{ willChange: 'transform' }}
-      // css={{
-      //   '.hoverable&:hover': {
-      //     svg: {
-      //       fill: color3,
-      //       stroke: color3,
-      //     },
-      //     '.bar': {
-      //       backgroundColor: color3,
-      //     },
-      //   },
-      // }}
-      onClick={handleClick}
-      style={style}
-    >
-      <animated.div
-        className="bar"
-        css={{
-          willChange: 'transform',
-          height: '20px',
-          width: (item.skill / 100) * BAR_MAX_WIDTH,
-          zIndex: -1,
-          transition: 'background-color 0.3s ease-out',
-          backgroundColor: color2,
-          position: 'absolute',
-          transformOrigin: 'right',
-          right: 0,
-          top: 8,
-        }}
-        style={barStyle}
-      />
-
-      <div
-        className="rounded-full p-2"
-        css={{
-          backgroundColor: 'white',
-          boxShadow: `0px 0px 5px 0px ${color1}`,
-        }}
-      >
-        <SkillIcon
-          skill={item.name}
+    <li className="relative" onClick={handleClick}>
+      <div className="flex flex-col items-end">
+        {/* <div
           css={{
-            transition: 'all 0.3s ease-out',
-            stroke: color1,
-            fill: color1,
+            height: '20px',
+            width: (item.skill / 100) * BAR_MAX_WIDTH,
+            zIndex: -1,
+            backgroundColor: color2,
+            position: 'absolute',
+            transformOrigin: 'right',
+            right: 0,
+            top: 8,
           }}
-        />
-      </div>
+        /> */}
 
-      <animated.div
-        className="text-xs whitespace-no-wrap text-right"
-        style={textStyle}
-      >
-        {item.name}
-      </animated.div>
-      {item.keywords.map((keyword, index) => {
-        const { transform } = transforms[index]
-
-        return (
-          <animated.div
-            key={keyword}
-            style={{
-              transform,
-            }}
+        <Flipped flipId={createIconFlipId(index)} stagger="item">
+          <div
+            className="rounded-full p-2"
             css={{
-              willChange: 'transform',
-              zIndex: -1,
+              backgroundColor: color3,
+              boxShadow: `0px 0px 5px 0px ${color1}`,
+              zIndex: 10,
             }}
-            className="absolute flex flex-col items-end"
           >
-            <div
-              className="rounded-full p-2"
+            <SkillIcon
+              skill={item.name}
               css={{
-                backgroundColor: 'white',
-                boxShadow: `0px 0px 5px 0px ${color1}`,
+                stroke: 'white',
+                fill: 'white',
               }}
-            >
-              <SkillIcon
-                skill={keyword}
-                css={{
-                  transition: 'stroke, fill 0.3s ease-out',
-                  stroke: color1,
-                  fill: color1,
-                }}
-              />
-            </div>
-            <div
-              className="text-xs whitespace-no-wrap text-right"
-              css={{ transition: 'opacity 0.3s ease-in' }}
-              style={{
-                opacity: isExpanded ? 1 : 0,
-              }}
-            >
-              {keyword}
-            </div>
-          </animated.div>
-        )
-      })}
-    </animated.div>
+            />
+          </div>
+        </Flipped>
+
+        <Flipped flipId={createTextFlipId(index)} stagger="text">
+          <div className="text-xs whitespace-no-wrap text-right">
+            {item.name}
+          </div>
+        </Flipped>
+      </div>
+    </li>
+  )
+}
+
+type HorizontalItemProps = {
+  index: number
+  item: any
+}
+function HorizontalItem({ index, item }: HorizontalItemProps) {
+  return (
+    <li>
+      <Flipped flipId={createIconFlipId(index)} stagger="item">
+        <div
+          className="rounded-full p-2"
+          css={{
+            backgroundColor: 'white',
+            boxShadow: `0px 0px 5px 0px ${color1}`,
+            zIndex: 10,
+          }}
+        >
+          <SkillIcon
+            skill={item.name}
+            css={{
+              stroke: color1,
+              fill: color1,
+            }}
+          />
+        </div>
+      </Flipped>
+    </li>
   )
 }
 
@@ -162,140 +147,82 @@ type SkillsProps = {
 function Skills({ resume }: SkillsProps) {
   const skills = resume.skills || []
 
-  const [isExpanded, setIsExpanded] = useState(false)
+  const [isVertical, setIsVertical] = useState(false)
   const [openStates, setOpenStates] = useState<boolean[]>(
     skills.map(() => false)
   )
-  const [isHovered, setIsHovered] = useState(false)
   const [activeIndex, setActiveIndex] = useState(0)
 
-  const [offsets, totalOffsets] = useMemo(() => {
-    const offsets: number[] = []
-    let totalOffsets = 0
-
-    for (let index = 0; index < skills.length; index++) {
-      let lastValue = index > 0 ? offsets[index - 1] : -1
-      let difference = 1
-      if (openStates[index - 1]) {
-        difference += skills[index - 1]?.keywords.length
-      }
-
-      offsets.push(lastValue + difference)
-      if (openStates[index]) {
-        totalOffsets += skills[index]?.keywords.length
-      } else {
-        totalOffsets += 1
-      }
-    }
-
-    return [offsets, totalOffsets]
-  }, [skills, openStates])
-
-  const animationValues = useSprings(
-    skills.length,
-    skills.map((skill, index) => ({
-      xOffset: isExpanded
-        ? 0
-        : offsets[index] * -ITEM_WIDTH * (isHovered ? 1 : 0.4),
-      yOffset: isExpanded ? offsets[index] * ITEM_HEIGHT : 0,
-      opacity: 1,
-      expanded: isExpanded ? 1 : 0,
-      delay: (index - activeIndex) * 50,
-    }))
-  )
-
-  const handleMouseEnter = useCallback(() => {
-    setIsHovered(true)
-  }, [setIsHovered])
-
-  const handleMouseLeave = useCallback(() => {
-    setIsHovered(false)
-  }, [setIsHovered])
-
   const handleExpand = useCallback(() => {
-    if (!isExpanded) {
-      setIsExpanded(true)
+    if (!isVertical) {
+      setIsVertical(true)
       setActiveIndex(0)
     }
-  }, [isExpanded, setIsExpanded, setActiveIndex])
+  }, [isVertical, setIsVertical, setActiveIndex])
 
   const handleCollapse = useCallback(() => {
-    if (isExpanded) {
-      setIsExpanded(false)
+    if (isVertical) {
+      setIsVertical(false)
       setActiveIndex(0)
       setOpenStates(openStates.map(() => false))
     }
-  }, [isExpanded, setIsExpanded, setActiveIndex, setOpenStates])
+  }, [isVertical, setIsVertical, setActiveIndex, setOpenStates, openStates])
 
   const handleClickItem = useCallback(
     index => {
       setOpenStates(
         openStates.map((isOpen, i) => {
-          if (i === index) {
-            return !isOpen
-          } else {
-            return isOpen
-          }
+          return i === index ? !isOpen : isOpen
         })
       )
       setActiveIndex(index)
     },
-    [openStates, skills, setOpenStates]
+    [openStates, setOpenStates, setActiveIndex]
   )
 
-  const height = isExpanded ? totalOffsets * ITEM_HEIGHT : ITEM_HEIGHT
-
   return (
-    <div
-      className={`relative ${isExpanded ? '' : 'cursor-pointer'}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleExpand}
+    <Flipper
+      flipKey={isVertical.toString() + openStates.join('')}
+      decisionData={activeIndex}
     >
-      <div
-        css={{
-          height,
-        }}
+      <ul
+        className={classNames('flex', {
+          'flex-col': isVertical,
+          'flex-row-reverse': !isVertical,
+        })}
+        onClick={isVertical ? handleCollapse : handleExpand}
       >
-        {isExpanded && (
+        {isVertical && (
           <AiOutlineClose className="cursor-pointer" onClick={handleCollapse} />
         )}
-      </div>
-      {skills.map((item, index) => {
-        const { xOffset, yOffset, opacity, expanded } = animationValues[index]
 
-        return (
-          <SkillItem
-            key={item.name}
-            item={item}
-            index={index}
-            onClick={isExpanded ? handleClickItem : undefined}
-            isExpanded={openStates[index]}
-            canHover={isExpanded && item.keywords?.length > 0}
-            style={{
-              zIndex: skills.length - index,
-              position: 'absolute',
-              right: 0,
-              top: 0,
-              transform: interpolate(
-                [xOffset, yOffset],
-                (x, y) => `translate3d(${x}px, ${y}px, 0)`
-              ),
-              opacity,
-            }}
-            barStyle={{
-              transform: expanded.interpolate(
-                yScale => `translate3d(${-30}px, 0, 0) scale(${yScale}, 1)`
-              ),
-              opacity: expanded,
-            }}
-            textStyle={{
-              opacity: expanded,
-            }}
-          />
-        )
-      })}
-    </div>
+        {skills
+          .map((item, index) => {
+            if (isVertical) {
+              const parent = (
+                <VerticalItem
+                  key={item.name}
+                  item={item}
+                  index={index}
+                  onClick={isVertical ? handleClickItem : undefined}
+                  isOpen={openStates[index]}
+                />
+              )
+              const children = openStates[index]
+                ? item.keywords.map(keyword => (
+                    <SubItem key={keyword} index={index} keyword={keyword} />
+                  ))
+                : []
+              return [parent, ...children]
+            } else {
+              return (
+                <HorizontalItem key={item.name} item={item} index={index} />
+              )
+            }
+          })
+          .flat()}
+      </ul>
+    </Flipper>
   )
 }
 
